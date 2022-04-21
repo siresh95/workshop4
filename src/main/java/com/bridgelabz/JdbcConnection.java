6015a76d846455f2a0f233c1d67184fd161ece88
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-22 01:13:46 +0530

    table added

--- src/main/java/com/bridgelabz/JdbcConnection.java
+++ src/main/java/com/bridgelabz/JdbcConnection.java
@@ -1,26 +1,31 @@
 package com.bridgelabz;
 
-// Java program to illustrate
-// Connecting to the Database
 import java.sql.*;
-import java.sql.Driver;
 import java.sql.DriverManager;
 
 public class JdbcConnection {
     public static void main(String args[]) {
-        String dbURL = "jdbc:mysql://localhost:3306/employee_details?useSSL=false";
+        Class.forName("com.mysql.jdbc.Driver");
+        String dbURL = "jdbc:mysql://localhost:3306/employee_details";
         String username = "root";
         String password = "Resh@308";
-
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(dbURL, username, password);
+            //create a query
+            String q ="create table employee ( firstname varchar(200) not null, lastnamename varchar(200) not null , " +
+                    "empid int not null" + "doj date not null )";
+          Statement stmt=conn.createStatement();
+          stmt.executeUpdate(q);
+          conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         if (conn != null) {
             System.out.println("Connected");
+            System.out.println("table created in database");
+
         }
     }
 }
